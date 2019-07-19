295b6921ec56212a0de88a7a54aed0427ea61803
Claire Song <songvely17@gmail.com>
2019-07-19 15:37:58 +0900

    add new_schedule xml

--- app/src/main/java/com/example/myapplication/RecomFragment.java
+++ app/src/main/java/com/example/myapplication/RecomFragment.java
@@ -9,7 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class RecomFragment extends Fragment {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class RecomFragment extends Fragment implements View.OnClickListener {
+    private FloatingActionButton button;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +24,14 @@ public class RecomFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_recom, container, false);
 
+        button = view.findViewById(R.id.btn_floating);
+        button.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
