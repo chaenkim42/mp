033ab04c25f5e65aff38ef540fbf898c50b8c37c
Claire Song <songvely17@gmail.com>
2019-08-01 16:59:15 +0900

    add kakao api map

--- app/src/main/java/com/example/myapplication/SearchMap.java
+++ app/src/main/java/com/example/myapplication/SearchMap.java
@@ -3,6 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.daum.mf.map.api.MapView;
 
 //지도로 검색결과 나오는 액티비티
 
@@ -12,5 +16,9 @@ public class SearchMap extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_map);
+
+        MapView mapView = new MapView(this);
+        ViewGroup container = (ViewGroup)findViewById(R.id.map_view);
+        container.addView(mapView);
     }
 }
