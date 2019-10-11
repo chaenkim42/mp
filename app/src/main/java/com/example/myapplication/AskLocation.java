5d26d9909f23082ecdedb563f098a4d5ae37a323
chaen <chaenkim42@gmail.com>
2019-10-12 00:58:32 +0900

    connect all activities
    when starting app, it starts from Splash and you can go to every activiteis via buttons

--- app/src/main/java/com/example/myapplication/AskLocation.java
+++ app/src/main/java/com/example/myapplication/AskLocation.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 
-public class AskLocation extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+import com.example.myapplication.Main.Main;
+
+public class AskLocation extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, View.OnClickListener {
 
     public String[] placeName = {
             "seoul","gyungju","tongyoung",
@@ -33,6 +37,9 @@ public class AskLocation extends AppCompatActivity implements CompoundButton.OnC
 
         int numberOfPlaces = placeName.length;
         Location[] locationList = new Location[numberOfPlaces];
+
+        Button nextBtn = findViewById(R.id.askLocation_nextPageBtn);
+        nextBtn.setOnClickListener(this);
 
         for(int i=0; i<numberOfPlaces; i++){
             String name = placeFileName[i];
@@ -112,5 +119,10 @@ public class AskLocation extends AppCompatActivity implements CompoundButton.OnC
             s+= String.valueOf(selectedBoolean[i]);
         }
         Log.e("select",s);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(AskLocation.this, Main.class));
     }
 }
