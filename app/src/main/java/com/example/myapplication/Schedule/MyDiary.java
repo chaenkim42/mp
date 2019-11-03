fdb75b0c7edf47e8425c9e8a9234808b6390e669
chaen <chaenkim42@gmail.com>
2019-11-04 00:54:37 +0900

    complete ui - mydiary
    - 추가적으로 필요한 부분 : 다이어리 시간, 대표이미지 연결, 날짜별 다이어리 조회

--- app/src/main/java/com/example/myapplication/Schedule/MyDiary.java
+++ app/src/main/java/com/example/myapplication/Schedule/MyDiary.java
@@ -8,28 +8,53 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.Main.MyPagePlansFragment;
 import com.example.myapplication.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyDiary extends AppCompatActivity {
+
+    public static class Diary{
+        public String title;
+        public String contents_text;
+        public List<String> photoIds = new ArrayList<>();
+        public Diary(String title, String contents_text, List<String> photoIds){
+            this.title = title;
+            this.contents_text = contents_text;
+            this.photoIds = photoIds;
+        }
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mydiary);
 
-//        RelativeLayout button = (RelativeLayout) findViewById(R.id.button);
+        List<Diary> tmpDiaryList = new ArrayList<>();
+        List<String> emptyString = new ArrayList<>();
+        Diary d = new Diary("호텔 조식", "생각보다 너무 맛있었음\n다시 올만한 곳\n아싸사사샷", emptyString);
+        Diary dd = new Diary("여수 세계 박람회", "너무 너무 재밌었당\n블라블라\n살라살라 쿵따리", emptyString);
+        Diary ddd = new Diary("한화 아쿠아 플라넷", "", emptyString);
+        Diary dddd = new Diary("공원", "", emptyString);
 
-//        button.setBackgroundColor(getResources().getColor(R.color.mainBlue));
-//
-//        FragmentManager fm = getSupportFragmentManager();
-//        FragmentTransaction transaction = fm.beginTransaction();
-//        transaction.add(R.id.button, new MyPagePlansFragment());
-//        transaction.commit();
-//        Fragment fragment1 = new MyPagePlansFragment();
-//        fragmentTransaction.add(R.id.mypage_container, fragment1);
-//        fragmentTransaction.commit();
+        tmpDiaryList.add(d);
+        tmpDiaryList.add(dd);
+        tmpDiaryList.add(ddd);
+        tmpDiaryList.add(dddd);
+        tmpDiaryList.add(d);
+        tmpDiaryList.add(dd);
+        tmpDiaryList.add(ddd);
+        tmpDiaryList.add(dddd);
+
+        RecyclerView recyclerView = findViewById(R.id.mydiary_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager( this, RecyclerView.VERTICAL, false));
+        recyclerView.setAdapter(new MyDiary_Adapter( tmpDiaryList,this));
+
 
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Schedule/MyDiary_Adapter.java
@@ -0,0 +1,60 @@
+package com.example.myapplication.Schedule;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyDiary_Adapter extends RecyclerView.Adapter {
+    List<MyDiary.Diary> diaryList = new ArrayList<>();
+    Context context;
+    public TextView title, contents_text;
+    public ImageView rep_photo;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View view){
+            super(view);
+        }
+    }
+
+    public MyDiary_Adapter(List<MyDiary.Diary> diaryList, Context context){
+        this.diaryList = diaryList;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // activity_mydiary_cardview layout을 화면에 뿌려주고 holder에 연결
+        View view;
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_mydiary_cardview, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        title = (TextView) holder.itemView.findViewById(R.id.mydiary_cardview_title);
+        contents_text = (TextView) holder.itemView.findViewById(R.id.mydiary_cardview_content);
+        rep_photo = (ImageView) holder.itemView.findViewById(R.id.mydiary_cardview_photobox);
+        title.setText(diaryList.get(position).title);
+        contents_text.setText(diaryList.get(position).contents_text);
+        rep_photo.setImageResource(R.drawable.photobox_empty);
+        rep_photo.setScaleType(ImageView.ScaleType.CENTER_CROP);
+    }
+
+    @Override
+    public int getItemCount() {
+        return diaryList.size();
+    }
+}
