427e8c854ca3be49a02399f826dedfbcba7cc812
Clarie Song <songvely17@gmail.com>
2019-11-19 17:24:13 +0900

    sign up

--- app/src/main/java/com/example/myapplication/Database/User.java
+++ app/src/main/java/com/example/myapplication/Database/User.java
@@ -7,7 +7,7 @@ public class User {
     public String sex;
     public int age;
 
-    public User(String email, String password, String name, String sex, int age){
+    public User(int age, String email, String name, String password, String sex){
         this.email = email;
         this.password = password;
         this.name = name;
--- app/src/main/java/com/example/myapplication/LogIn.java
+++ app/src/main/java/com/example/myapplication/LogIn.java
@@ -5,6 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,20 +15,29 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.myapplication.Database.User;
+import com.example.myapplication.Main.Main;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
 
 public class LogIn extends AppCompatActivity {
     DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
     DatabaseReference user_ref = myRef.child("user");
-    DatabaseReference email_Ref = user_ref.child("email");
-
-    String email, password;
-    EditText email_field, password_field;
+    EditText email_field;
+    EditText password_field;
+    HashMap<String, String> s;
+    public static final ArrayList<User> users= new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,52 +46,68 @@ public class LogIn extends AppCompatActivity {
 
         Button login = findViewById(R.id.btn_login);
         Button checking = findViewById(R.id.btn_print);
-        EditText email_field = findViewById(R.id.field_email);
-        EditText password_field = findViewById(R.id.field_password);
+        email_field = findViewById(R.id.field_email);
+        password_field = findViewById(R.id.field_password);
+
+       s = new HashMap<>();
+
+        user_ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                Iterator<DataSnapshot> child = dataSnapshot.getChildren().iterator();
+
+                while(child.hasNext()){
+                    DataSnapshot temp = child.next(); //각각의 유저 객체
+
+                    // 파싱
+                    StringTokenizer tk = new StringTokenizer(temp.getValue().toString(), ", ");
+                    String[] arr = new String[5];
+                    int i=0;
+                    while(tk.hasMoreTokens()){
+                        String[] dummy = tk.nextToken().split("=");
+                        arr[i] = dummy[1];
+                        i++;
+                    }
+                    arr[4] = arr[4].substring(0, arr[4].length()-1);
+
+                    //유저 객체 생성
+                    User user = new User(Integer.parseInt(arr[0]), arr[1], arr[2], arr[3], arr[4]);
+                    users.add(user);
+
+                    // set 이용해서 회원가입 진행
+                    s.put(arr[4], arr[0]);
+//                    Log.d("e: ", arr[4]);
+//                    Log.d("p: ", arr[0]);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
     }
 
     public void onClick(View view){
         switch (view.getId()){
             case R.id.btn_login:
-                email = email_field.getText().toString();
-                password = password_field.getText().toString();
+                String given_email = email_field.getText().toString();
+                String given_password = password_field.getText().toString();
 
-                // 이메일 패스워드 맞으면 main으로 넘어감
-                myRef.addChildEventListener(new ChildEventListener() {
-                    @Override
-                    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-                        if(dataSnapshot.getKey().equals("email")){
-                            if(dataSnapshot.getValue().equals(email)){
-                                Toast.makeText(getApplicationContext(), "email correct", Toast.LENGTH_SHORT).show();
-                            }
-                        }
+                if(s.keySet().contains(given_email)){
+                    if(s.get(given_email).equals(given_password)){
+                        startActivity(new Intent(getApplicationContext(), Main.class));
                     }
+                }else{
+                    Log.d("error: ", "noooo");
+                }
 
-                    @Override
-                    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-                    }
-
-                    @Override
-                    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-                    }
-
-                    @Override
-                    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
-
-                // 아니면 토스트
                 break;
+
             case R.id.btn_ask_sign_up:
                 Intent intent = new Intent(LogIn.this, SignUp.class);
                 startActivity(intent);
